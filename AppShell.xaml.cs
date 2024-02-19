01bca6f331c7bc766d55e5fc5f126a6b3c4d35e0
User <User@LAB3-3>
2024-02-19 13:13:18 +0200

    //

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace MonkeysMVVM;
+﻿using MonkeysMVVM.Views;
+
+namespace MonkeysMVVM;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
 		InitializeComponent();
+		Routing.RegisterRoute("FindMonkeyByLocation", typeof(FindMonkeyByLocationPage));
+		Routing.RegisterRoute("MonkeysPage", typeof(MonkeysPage));
+		Routing.RegisterRoute("ShowMonkeyView", typeof (ShowMonkeyView));
 	}
 }
--- ViewModels/MonkeysPageViewModel.cs
+++ ViewModels/MonkeysPageViewModel.cs
@@ -3,6 +3,7 @@ using MonkeysMVVM.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,15 +15,26 @@ namespace MonkeysMVVM.ViewModels
     {
         private bool isRefreshing;
         public bool IsRefreshing { get => isRefreshing; set { isRefreshing = value; OnPropertyChanged(); } }
+        public Monkey SelectedMonkey {  get; set; } 
         public ObservableCollection<Monkey> Monkeys { get; set; }
         public Monkey Monkey { get; set; }
         public ICommand LoadMonkeysCommand { get; private set; }
+        public ICommand NavigateToShowMonkey {  get; private set; }
 
         public MonkeysPageViewModel()
         {
             Monkeys= new ObservableCollection<Monkey>();
 
             LoadMonkeysCommand = new Command(async () => await LoadMonkeys());
+            NavigateToShowMonkey = new Command(async () => await GoToMonkeyPage());
+        }
+
+        private async Task GoToMonkeyPage()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("monkey", SelectedMonkey);
+            await AppShell.Current.GoToAsync("ShowMonkeyView",data);
+           
         }
 
         private async Task LoadMonkeys()
@@ -36,5 +48,6 @@ namespace MonkeysMVVM.ViewModels
             }
             IsRefreshing= false;
         }
+
     }
 }
